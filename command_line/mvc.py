78f89880d9887d0d5d237e8dd70e5bb07a16a3e0
Ayi_n <sourlemon2270@gmail.com>
2023-01-10 14:36:39 -0800

    __init__.py added

--- command_line/mvc.py
+++ command_line/mvc.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
-from iotbx.cli_parser import run_program
-from protein_visualisation.programs.mvc import Program
+from iotbx.cli_parser import run_program 
+import sys
+print (sys.path)
+import phenix
+from phenix.programs.mvc import Program
 
 if (__name__ == '__main__'):
   results = run_program(program_class=Program)
--- programs/mvc.py
+++ programs/mvc.py
@@ -1,3 +1,9 @@
 from libtbx.program_template import program_template
 
 class Program(program_template):
+	datatypes = ['model']
+
+	def validate(self): pass
+	def run(self): print("hello")
+
+	
