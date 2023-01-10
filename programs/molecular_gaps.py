c21014e978cb4295a8a03613c00afdd7fea8851d
CS0607 <98332624+CS0607@users.noreply.github.com>
2023-01-10 09:07:32 -0800

    Proper Mac setup

--- programs/molecular_gaps.py
+++ programs/molecular_gaps.py
@@ -1,7 +1,24 @@
-from libtbx.program_template import program_template
+from libtbx.program_template import ProgramTemplate
 
-class Program(ProgramTemplate);
+class Program(ProgramTemplate):
 
-	datatypes = ['model', 'phil']
+  description = '''
+    molecular gaps need a model file'''
+
+  datatypes = ['model', 'phil']
+
+  def validate(self):
+  	pass
+
+  def run(self):
+  	print('hi')
+  	model = self.data_manager.get_model()
+  	print('model',dir(model))
+  	hierarchy = model.get_hierarchy()
+  	for residue_group in hierarchy.residue_groups():
+  		print(residue_group.id_str())
+
+  def results(self):
+  	return self.results
 
 	
