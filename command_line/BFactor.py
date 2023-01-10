15bda4aa10d02a292f1b3869308ff7643513fc0f
Lijia Yang <lijiayang@c-e4-41-e0-10-f5.dhcp.lbnl.us>
2023-01-10 13:56:17 -0800

    yang

--- command_line/BFactor.py
+++ command_line/BFactor.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
 from iotbx.cli_parser import run_program
-from protein_visualization.programs.BFactor import Program
+import protein_visualisation
+from protein_visualisation.programs.BFactor import Program
 
 if (__name__ == '__main__'):
   results = run_program(program_class=Program)
--- programs/BFactor.py
+++ programs/BFactor.py
@@ -4,3 +4,18 @@ class Program(ProgramTemplate):
 
   datatypes = ['model','phil']
 
+  def validate(self): pass
+
+  def run(self):
+    print('hi')
+    model = self.data_manager.get_model()
+    print('model',dir(model))
+    hierarchy = model.get_hierarchy()
+    for residue_group in hierarchy.residue_groups():
+      print(residue_group.id_str())
+
+  def results(self):
+    return self.results
+
+
+
