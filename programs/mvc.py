4434c3609615c23c62a112f803c8a73b02ab0919
Ayi_n <sourlemon2270@gmail.com>
2023-01-18 13:34:36 -0500

    ayins updated cwode

--- programs/mvc.py
+++ programs/mvc.py
@@ -7,16 +7,19 @@ import wx
 from iotbx.cli_parser import run_program
 import sys
 import random
+import BFactor
+import ligand
+import molecular_gaps
 
 #-------------------------------------------------------------------------------------------------------------------
 #tools
-
 class MyError:
 	def __init__(self, message):
 		self.message = message
 
 def linToPol(x,y):
 	if(y == 0):
+        #returns an error if we divide by zero
 		e = MyError("y cannot be 0")
 		print(e.message)
 		return e
@@ -28,6 +31,9 @@ def polToLin(r,theta):
 	point = wx.Point(r*math.cos(theta), r*math.sin(theta))
 	return point
 
+def toRad(degree):
+    return degree*math.pi/180
+
 def rainbow(prop):
     if(prop > 1 or prop < 0):
         return [0,0,0]
@@ -35,12 +41,8 @@ def rainbow(prop):
     b = 255*(1-prop)
     return [r,0,b]
 
-#----------------------------------------------------------------------------------------------------------------------
-#threading
-
 #---------------------------------------------------------------------------------------------------------------------
 #window stuff
-
 class Mywin(wx.Frame): 
             
     def __init__(self, parent, title): 
@@ -49,19 +51,24 @@ class Mywin(wx.Frame):
           
     def InitUI(self): 
         print("initUI")
-        # self.up = True
+        self.bFactorButton = False
         self.prop = 0
         self.numRects = 0
         self.rectColors = []
 
-        self.bg_color = "pink"
+        self.bg_color = [240,240,240]
         self.panel = wx.Panel(self)
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         
         ID_NO = wx.NewId()
-        button = wx.Button(self, ID_NO, 'DO NOT PRESS THIS BUTTON', pos=(20,40))
+        button = wx.Button(self, ID_NO, 'run', pos=(20,40))
         button.Bind(wx.EVT_BUTTON, self.OnClicked, id=ID_NO)
         self.sizer.Add(button, 1, wx.ALL|wx.EXPAND, 0)
+
+        ID_SWITCH = wx.NewId()
+        self.newBut = wx.Button(self, ID_SWITCH, 'switch', pos = (100, 40))
+        self.newBut.Bind(wx.EVT_BUTTON, self.switchButton, id=ID_SWITCH)
+        self.sizer.Add(self.newBut, 1, wx.ALL|wx.EXPAND, 0)
 
         self.panel.SetSizer(self.sizer)
         self.Centre()
@@ -69,55 +76,97 @@ class Mywin(wx.Frame):
 
         self.Bind(wx.EVT_PAINT, self.OnPaint) 
 
-    def OnClicked(self, event):
-        print("in on clicked rn")
-        self.myWinRun()
-        if(self.results is None):
-            self.results = 0
-        self.numRects = self.results
-        print(self.numRects)
-        for num in range(0, self.numRects, 1):
-            self.prop = random.random()
-            self.rectColors.append(rainbow(self.prop))
-        # self.bg_color = rainbow(self.prop)
-        # if(self.up):
-        #     self.prop += .05
-        #     print(self.prop)
-        # else:
-        #     self.prop -= .05
-        #     print(self.prop)
-        # if(self.prop > 1):
-        #     self.prop = 1
-        #     self.up = False
-        # elif(self.prop < 0):
-        #     self.prop = 0
-        #     self.up = True
+    def switchButton(self, event):
+        print("HELLO I GOT HERE MOM< I AM SO COOL")
+        self.rectColors = []
+        if(self.bFactorButton):
+            self.bFactorButton = False
+            self.OnClicked(None)
+        else:
+            print("got here")
+            self.bFactorButton = True
+            self.OnClicked(None)
         self.Refresh()
         self.Centre()
         self.Show(True)
+
+    def getColorsBFactor(self):
+        returnList = []
+        for key, item in self.bFactorData.items():
+            for k2, i2 in item.items():
+                if(k2 == "res"):
+                    returnList.append(i2/100)
+        return returnList
+
+    def getColorsGaps(self):
+        returnList = []
+        for key, item in self.gaps.items():
+            for k2, i2 in item.items():
+                for k3, i3 in i2.items():
+                    if(i3 == False):
+                        returnList.append(1)
+                        returnList.append(0)
+                    else:
+                        returnList.append(0)
+        print(returnList)
+        return returnList
+
+    def OnClicked(self, event):
+        print("in on clicked rn")
+        self.myWinRun()
+        if(self.bFactorButton):
+            colors = self.getColorsBFactor()
+        else:
+            colors = self.getColorsGaps()
+        if(self.results is None):
+            self.results = 0
+        self.numRects = self.results
+        for num in range(0, self.numRects, 1):
+            if(num >= len(colors)):
+                return
+            self.prop = colors[num]
+            self.rectColors.append(rainbow(self.prop))
+        self.Refresh()
+        self.Centre()
+        self.Show(True)
+
+    # def yuh(self):
+    #     totalTheta = toRad(270)
+    #     boxAngle = totalTheta/self.rectColors
+    #     pointList = []
+    #     for i, col in self.rectColors:
+    #         print(i, col)
+    #         # pointList.append([])
+
 
     def OnPaint(self, e): 
         print("on paint")
         dc = wx.PaintDC(self) 
         dc.SetBackground(wx.Brush(self.bg_color))  
         dc.Clear() 
-
         for i,col in enumerate (self.rectColors):
             color = wx.Colour(*tuple(col))
             b = wx.Brush(color)
             dc.SetBrush(b)
-            dc.DrawRectangle(i*50, 100, 50,50)
+            row = math.floor(i/20)+2
+            column = i%20
+            xPos = column*50
+            yPos = row*50
+            dc.DrawRectangle(xPos, yPos, 50,50)
+        # print("YUH")
+        # self.yuh()
 
 
     def myWinRun(self):
         print("myWinRun")
-        self.results = run_program(program_class=Program, args=[sys.argv[1]])
-        print("these are the results")
-        print(self.results)
+        pdbfile = sys.argv[1]
+        self.results = run_program(program_class=Program, args=[pdbfile])
+        self.bFactorData = run_program(program_class=BFactor.Program, args=[pdbfile])
+        print("CONNOR")
+        self.gaps = run_program(program_class=molecular_gaps.Program, args=[pdbfile])
   
 #-------------------------------------------------------------------------------------------------------------------		
 #program stuff
-
 class Program(ProgramTemplate):
     datatypes = ['model', 'phil']
   
@@ -127,17 +176,9 @@ class Program(ProgramTemplate):
         print("i am running the program!")
     	model = self.data_manager.get_model()
     	hierarchy = model.get_hierarchy()
-        #print(dir(hierarchy))
-        #this is only the first chain which is an issue for later </3
-        #dictionary with a key of a chain, and for each chain we put interests
         for chain in hierarchy.chains():
             numRects = chain.residue_groups_size()
         self.results = numRects
-    	# for residue_group in hierarchy.residue_groups():
-    	# 	for atom in residue_group.atoms():
-    	# 		print(atom.quote())
-        print('RESY')
-        print(self.results)
 
     def get_results(self):
         print("returing stuff", self.results)
@@ -150,7 +191,6 @@ class Program(ProgramTemplate):
     	dc.AddArc(300, -45, 225)
 # ----------------------------------------------------------------------------------------------------------------------------
 # running stuff
-
 if __name__=='__main__':
     ex = wx.App() 
     w = Mywin(None,'Drawing demo')
